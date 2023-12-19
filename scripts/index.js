17ba1753f9ec55badebfda84d6f22a93c9306a20
donny333 <125969039+donny333@users.noreply.github.com>
2023-12-19 11:28:45 +0200

    connected to restcountries.com

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 /* EVENT DELEGATION */
 /* Paselectinami visi vaikiniai elemetnai */
 
-document
-    .querySelector('#myList')
-    .addEventListener('click', function(e){
-        console.log(`You clicked on item ${e.target.innerHTML}`)
-    })
+// document
+//     .querySelector('#myList')
+//     .addEventListener('click', function(e){
+//         console.log(`You clicked on item ${e.target.innerHTML}`)
+//     })
 
 /* END*/
 
 
 /* CLOSURE */
 
-function outerFunction(outerVariable){
-    return function innerFunction(innerVariable){
-        console.log('Outer variable: ' + outerVariable);
-        console.log('Inner variable: ' + innerVariable);
-    }
-}
+// function outerFunction(outerVariable){
+//     return function innerFunction(innerVariable){
+//         console.log('Outer variable: ' + outerVariable);
+//         console.log('Inner variable: ' + innerVariable);
+//     }
+// }
 
-const newFunction = outerFunction('outer')
-newFunction('inner')
+// const newFunction = outerFunction('outer')
+// newFunction('inner')
 
 /* END */
 
@@ -38,3 +38,23 @@ AJAX is a web developement techique.
 xhr - XMLHTTPRequest
 
 */
+
+function showCountries(){
+    let xhr = new XMLHttpRequest;
+
+    xhr.open('GET', 'https://restcountries.com/v3.1/all', true);
+
+    xhr.onload = function(){
+        if (xhr.status) {
+            console.log('Success')
+        } else {
+            console.log('Fail')
+        }
+    }
+
+    xhr.send();
+}
+
+document
+    .querySelector('#countriesButton')
+    .addEventListener('click', showCountries)
